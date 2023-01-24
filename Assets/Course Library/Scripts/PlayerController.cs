51e052f8f09b581fc2420f0d1860f23aedecd803
CarlChapman1 <27chapmancl@marshallisd.com>
2023-01-24 10:03:15 -0600

    Added unity code

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponet<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput =Input.GetAxis("Vertial")
+    }
+}
